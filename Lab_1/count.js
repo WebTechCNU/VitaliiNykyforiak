bc7ddbd2447b09ea81c7f860490ce3cd64457340
VitaliiNykyforiak <125277946+VitaliiNykyforiak@users.noreply.github.com>
2023-02-15 22:00:51 +0200

    complete calculator

--- Lab_1/count.js
+++ Lab_1/count.js
@@ -1,4 +1,4 @@
-function add() {
+function addition() {
     let inputA = document.querySelector("#input-a").value;
     let inputB = document.querySelector("#input-b").value; 
 
@@ -10,12 +10,35 @@ function add() {
 function multiply() {
     let inputA = document.querySelector("#input-a").value;
     let inputB = document.querySelector("#input-b").value;
-    
+
     let resultMulti = Number(inputA) * Number(inputB);
     let outputMulti = document.querySelector(".multi-res");
     outputMulti.innerHTML = resultMulti;
 }
 
-button = document.querySelector("#button");
-button.addEventListener("click", add);
-button.addEventListener("click", multiply);
+function division() {
+    let inputA = document.querySelector("#input-a").value;
+    let inputB = document.querySelector("#input-b").value;
+
+    let resultDivi = Number(inputA) / Number(inputB);
+    let outputDivi = document.querySelector(".divi-res");
+    outputDivi.innerHTML = resultDivi;
+}
+
+function subtraction() {
+    let inputA = document.querySelector("#input-a").value;
+    let inputB = document.querySelector("#input-b").value;
+
+    let resultSubt = Number(inputA) - Number(inputB);
+    let outputSubt = document.querySelector(".subtr-res");
+    outputSubt.innerHTML = resultSubt;
+}
+
+let addButton = document.querySelector("#adding-button");
+addButton.addEventListener("click", addition);
+let multiButton = document.querySelector("#multi-button");
+multiButton.addEventListener("click", multiply);
+let diviButton = document.querySelector("#divi-button");
+diviButton.addEventListener("click", division);
+let subtrButton = document.querySelector("#subtr-button");
+subtrButton.addEventListener("click", subtraction);
